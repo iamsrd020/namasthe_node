a4c6201efbc68a3a5c494ed95bde6bc840ebde6a
DARSHAN S R <darshansr403@gmail.com>
2025-09-07 13:49:02 +0530

    need to continue

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+require("./xyz");
+
+
 var name = "Darshan";
 
 var a = 10;
 
 var b = 20;
 
-console.log(name);
-console.log(a + b);
+// console.log(name);
+// console.log(a + b);
 
-console.log(global);
-console.log(this); //empty object in node js
-console.log(globalThis); //same as global
-console.log(globalThis === global); //true
+// console.log(global);
+// console.log(this); //empty object in node js
+// console.log(globalThis); //same as global
+// console.log(globalThis === global); //true

3842848990654b567863e5a8428fc07a17552a90
DARSHAN S R <darshansr403@gmail.com>
2025-09-07 13:41:17 +0530

    some basic concepts

---
+++ app.js
@@ -0,0 +1,13 @@
+var name = "Darshan";
+
+var a = 10;
+
+var b = 20;
+
+console.log(name);
+console.log(a + b);
+
+console.log(global);
+console.log(this); //empty object in node js
+console.log(globalThis); //same as global
+console.log(globalThis === global); //true
